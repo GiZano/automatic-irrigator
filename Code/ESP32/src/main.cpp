4605ddad6064cbbb88795da73e3bd2b33f54efba
GiZano <giozanotti07@gmail.com>
2025-11-11 20:12:50 +0100

    ON-OFF Led with ESP32-C3 to learn how to configure and program it

--- Code/ESP32/src/main.cpp
+++ Code/ESP32/src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
 
+// Costants
+int LED = 2;
+
 void setup() {
-  // setuo code
+  delay(2000); // inizialization time
+  
+  Serial.begin(115200);
+  Serial.println("=== ESP32-C3 RUNNING ===");
+  Serial.println("Setup completed!");
+  
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // main code
+  digitalWrite(LED, HIGH);
+  Serial.println("LED ON - " + String(millis()));
+  delay(1000);
+  
+  digitalWrite(LED, LOW);
+  Serial.println("LED OFF - " + String(millis()));
+  delay(1000);
 }
-
